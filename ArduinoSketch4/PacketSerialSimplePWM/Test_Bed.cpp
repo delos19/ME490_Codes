c1b79d02c21ad6b416610fd9686ad02712d94112
jnwiggins@sdsu.edu <jnwiggins@sdsu.edu>
2019-12-01 17:39:33 -0800

    Modified to work with digital values
    Created branch to make modifications suggested by chris for performance
    enhancement
    Changed bang-bang controll loop to use digital values for position
    resolution

--- ArduinoSketch4/PacketSerialSimplePWM/Test_Bed.cpp
+++ ArduinoSketch4/PacketSerialSimplePWM/Test_Bed.cpp
@@ -20,7 +20,7 @@ float val = 0;
 int valPerc = 0;
 int potPin = A0;
 float pos = 0;
-float threshold = 1.0;
+int threshold = 1;
 float setPoint = 0;
 int spiCSPin = 10;
 float raw;
@@ -34,7 +34,7 @@ void setup() {
 	pinMode(ledPin, OUTPUT);			//Initializes the ledPin to be an output
 	pinMode(potPin, INPUT);				//Defines the potentiometer pin as an input
 	pinMode(motor,OUTPUT);				//Defines the pin connecting the PCB to the Roboclaw (PWM channel, in the servo class claw) as output
-	threshold = 0.35;				//Sets the threshold value to read in between
+	threshold = 10;					//Sets the threshold value to read in between
 	while (CAN_OK != CAN.begin(CAN_1000KBPS)) {	//Initializes the CAN connection at 1 Mbps
 		Serial.println("CAN BUS Init Failed");
 		delay(100);
@@ -54,15 +54,16 @@ void loop() {
 	unsigned char len = 0;				//Initialized the length indicator
 	unsigned char buf[8];				//Initialize the buffer array of size 8
 	if(CAN_MSGAVAIL == CAN.checkReceive()) {	//Run the check on the CAN bus line. It verifies that signal is being received
-		CAN.readMsgBuf(&len, buf);		//Saves the length of the data to integer  'len' and the data array to the buffer array 'buf'
+		CAN.readMsgBuf(&len, buf);		//Saves the length of the data to integer 'len' and the data array to the buffer array 'buf'
 		unsigned long canId = CAN.getCanId();	//Reads each CAN address and assigns it to the unsigned long val canId
 		val = analogRead(potPin);		//Reads the voltage of the potentiometer and assigns it a value between 0 and 1023
-		pos = map(val, 0, 1023, 0, 8);		//Gets the position of the actuator by mapping the value read from the potentiometer between 0 in to 8 in
+		pos = val;				//Gets the position of the actuator by mapping the value read from the potentiometer between 0 in to 8 in
 		raw = buf[0];				//Stores the value at indices 0 of the buffer array to the integer 'Raw' 
-		setPoint = map(raw, 0, 255, 0, 5);	//Remaps the read value from the buffer indices to read as a length that maximizes at the maximum actuation length of the system
+		//raw = 127;
+		setPoint = map(raw, 0, 255, 0, 530);	//Remaps the read value from the buffer indices to read as a length that maximizes at the maximum actuation length of the system
 	}
-	
 							// TODO: Replace Bang-Bang control loop with PID
+	//setPoint = rpmL(raw);				// TODO: Update to reflect relationship of RPM to inches. Remove above setpoint when this change occurs.
 
 	//Begin Bang-Bang control loop
 	if (pos >= (setPoint - threshold) && pos <= (setPoint + threshold)) { //If read length is inside the threshold, send 1500 == STOP
@@ -70,14 +71,21 @@ void loop() {
 		delay(50);
 		Serial.print(pos);
 		digitalWrite(ledPin, HIGH);
-	} else if (pos > (setPoint - threshold)) {	//If it is greater than the setpoint, go backwards at full speed
+	} else if (pos > (setPoint - threshold)) {	//If it is greater than the setPoint, go backwards at full speed
 		claw.writeMicroseconds(2300);
 		delay(50);
 		Serial.print(pos);
-	} else if (pos < (setPoint + threshold)) {	//If it is greater than the setpoint, go forwards at full speed
+	} else if (pos < (setPoint + threshold)) {	//If it is greater than the setPoint, go forwards at full speed
 		claw.writeMicroseconds(1000);
 		delay(50);
 		Serial.print(pos);
 	}
 }
 
+/* void rpmL(raw){
+	float rpm = map(raw, 0, 255, 0, 15353);
+	float inch = rpm/10000;				// TODO: Replace with the correct relationship to RPM
+	int digi_inch = map(inch, 0, 8, 0, 1023);
+	return digi_inch;
+		
+} */
